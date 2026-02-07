a52c4c98a18cbc01359c98b41fa6881aaa1b2a21
Rafa-x64 <alvarezrafaelat@gmail.com>
2026-02-06 20:13:40 -0400

    chore: limpieza total y unificación de monorepo

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import './assets/main.css' // Tus estilos aquí
-import 'bootstrap' // Esto importa el JS de Bootstrap
-import App from './App.vue'
-
-createApp(App).mount('#app')
