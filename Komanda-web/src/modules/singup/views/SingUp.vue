4f547dd0e73da2944e1320e5a120a75920a82b23
Rafa-x64 <alvarezrafaelat@gmail.com>
2026-02-16 15:17:06 -0400

    UI de registro al 10%

--- Komanda-web/src/modules/singup/views/SingUp.vue
+++ Komanda-web/src/modules/singup/views/SingUp.vue
@@ -3,8 +3,52 @@ import { ref } from 'vue';
 
 </script>
 <template>
-    hola mundo
+    <div class="background-wrapper">
+        <div class="container-fluid content-layer d-flex flex-column justify-content-center align-items-center">
+            <div class="row w-75 p-1 d-md-none">
+                <!--para movil-->
+                <div class="col-12">Col 1</div>
+                <div class="col-12">Col 2</div>
+            </div>
+            <div class="row w-75 p-1 d-none d-md-flex">
+                <!--para pc y tablet-->
+                <div class="col-6">Col 1</div>
+                <div class="col-6">Col 2</div>
+            </div>
+        </div>
+    </div>
 </template>
 <style scoped>
+.background-wrapper {
+    position: relative;
+    min-height: 100vh;
+    overflow: hidden;
+}
 
+.background-wrapper::before {
+    content: '';
+    position: absolute;
+    inset: 0;
+    background-image: url(../../../assets/singin-singup-image.jpeg);
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    filter: blur(5px);
+    transform: scale(1.1);
+    z-index: 0;
+}
+
+.content-layer {
+    position: relative;
+    z-index: 1;
+}
+
+[class^="col"] {
+    border: 1px solid blue;
+    background: rgba(255, 255, 255, 0.7);
+}
+
+[class^="row"] {
+    border: 1px solid red;
+}
 </style>
